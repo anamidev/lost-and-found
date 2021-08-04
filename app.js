047103ea653099fd656f858077b1501c8057355d
lil-m00ha <minaevaan95@gmail.com>
2021-08-04 19:42:12 +0300

    Release 0 - добавлены routes Добавлены routes, прописаны основные ручки, добавлены комментарии для ручек, что они должны делать

---
+++ app.js
@@ -0,0 +1,54 @@
+// основные пакеты для работы приложения
+const express = require('express');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const cookieParser = require('cookie-parser');
+const hbs = require('hbs');
+
+// логгер для чтения команд на сервере и путь
+const morganLogger = require('morgan');
+const path = require('path');
+
+// импорт роутеров
+const mainPageRouter = require('./routes/mainpage');
+const registrationRouter = require('./routes/registration');
+const loginRouter = require('./routes/login');
+const logoutRouter = require('./routes/logout');
+const profileRouter = require('./routes/profile');
+
+// объявление приложения и указание порта
+const app = express();
+const PORT = 3000;
+
+// подключение движка отображения
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
+
+// мидлвер
+// логгер, статик для файлов, декодинг тела формы, декодинг джсона, парсер куков,
+// подключение сессии с использованием хранилища файлов сессий
+app.use(morganLogger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+  secret: 'alOjV68Hbs3zpL016FbsuGd5',
+  resave: false,
+  saveUninitialized: false,
+  name: 'session',
+  cookie: { secure: false, httpOnly: true },
+  store: new FileStore({}),
+}));
+
+// использование роутеров под определенные адреса
+app.use('/', mainPageRouter);
+app.use('/registration', registrationRouter);
+app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
+app.use('/profile', profileRouter);
+
+app.listen(PORT, () => {
+  console.log('Server is up!');
+});
---
+++ routes/post.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const db = require('../db/models/index');
+
+const router = express.Router();
+
+router.get('/:id', async (req, res) => {
+  // отрисовка страницы поста с информацией о создателе и пользователях,
+  // которые сделали claim
+  const { id } = req.params;
+  const post = await db.Post.findOne({
+    where: { id },
+    // include db.User
+  });
+  // проверка наличия сессии, через middleware
+  // если сессия есть, то отображать кнопку claim
+  // проверка, если текущий пользователь сделал claim на посту или нет
+  const postClaimers = await db.Claim.findAll({
+    // attributes: ['email'],
+    // where: { post_id: id },
+    // include db.User
+  });
+  if (postClaimers.includes(req.session.userEmail)) {
+    // вместо res.send отправить json
+    res.send('отправляем объект для статуса кнопки, чтобы добавить ей класс для выключенной стилизации');
+  } else {
+    // вместо res.send отправить json или статус ответа через res.status
+    res.send('post page');
+  }
+});
+
+router.post('/:id', async (req, res) => {
+  const { id } = req.params;
+  const claimedPost = await db.Claim.findOne({
+    where: { claimer_id: req.session.userId },
+  });
+  if (claimedPost) {
+    // вместо res.send отправить json или статус ответа через res.status
+    res.send('this post has already been claimed');
+  } else {
+    await db.Claim.create({ post_id: id, claimer_id: req.session.userId });
+    // сделать запись, отправить статус успеха, возможно сделать редирект на страницу этого же поста
+    res.send('post has been claimed');
+  }
+});
+
+module.exports = router;
